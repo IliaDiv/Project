6e217a980e6dd8b7aaf43683e3cc9e2a4d54ce1b
IliaDiv <divinskyilia@gmail.com>
2025-09-10 12:34:57 +0300

    changed package.json and added test.js

---
+++ test/example.test.js
@@ -0,0 +1,11 @@
+const assert = require('assert');
+
+describe('Basic Math Test', function() {
+  it('should return 4 when adding 2 + 2', function() {
+    assert.strictEqual(2 + 2, 4);
+  });
+
+  it('should return 0 when subtracting 2 - 2', function() {
+    assert.strictEqual(2 - 2, 0);
+  });
+});
